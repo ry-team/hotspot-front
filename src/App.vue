54c70cffe3265d976d25386fc3633a8c28a7a8e1
Timur <fearintino@mail.ru>
2017-06-01 12:29:04 +0500

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style scoped>
+@import "./assets/main.css";
+  .abs {
+    display: flex;
+  }
+</style>
